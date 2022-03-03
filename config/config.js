22a00375739bc746e644d2308f12a1b3298d680a
FoongL <leung.foong@gmail.com>
2022-03-03 10:38:01 +0800

    updated db connection config

--- config/config.js
+++ config/config.js
@@ -1,10 +1,42 @@
 require(`dotenv`).config()
+
+
+let development
+let production
+
+if (process.env.DATABASE_URL) {
+  development = {
+    "use_env_variable": "DATABASE_URL",
+    dialect: process.env.DB_DIALECT,
+    dialectOptions: {
+      "ssl": {
+        "rejectUnauthorized": false
+      }
+    }
+  }
+} else {
+  development = {
+    username: process.env.PG_USER, // postgres
+    password: process.env.PG_PASSWORD, // postgres
+    database: process.env.PG_DB_NAME, // database namne
+    host: process.env.PG_HOST, // host I.P.
+    dialect: process.env.DB_DIALECT, // PG
+  }
+}
+
+
+production = {
+  "use_env_variable": "DATABASE_URL",
+  dialect: process.env.DB_DIALECT,
+  dialectOptions: {
+    "ssl": {
+      "rejectUnauthorized": false
+    }
+  }
+}
+
+
 module.exports = {
-    development: {
-      username: process.env.PG_USER, // postgres
-      password: process.env.PG_PASSWORD, // postgres
-      database: process.env.PG_DB_NAME, // database namne
-      host: process.env.PG_HOST, // host I.P.
-      dialect: process.env.DB_DIALECT, // PG
-    },
-  };
+  development,
+  production
+};
